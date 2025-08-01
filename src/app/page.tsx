55582a04b3b023e209d53b8f1071c63fed5a74b1
Hamza Munir <infoathamza@gmail.com>
2025-08-01 18:26:17 +0000

    Set up Firebase Backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,10 +14,7 @@ import { pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.js',
-  import.meta.url,
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 declare const __app_id: string;
 
