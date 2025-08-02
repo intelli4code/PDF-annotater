71cc6152e7cdcec4a8fa2c2ed1e95ca0ea25dde2
Hamza Munir <infoathamza@gmail.com>
2025-08-02 02:47:30 +0000

    NEXT_PUBLIC_SUPABASE_URL=https://zfohraoldbaubkrjppec.supabase.co NEXT_P

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'your-supabase-url-placeholder'
-const supabaseAnonKey = 'your-supabase-anon-key-placeholder'
+const supabaseUrl = 'https://zfohraoldbaubkrjppec.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpmb2hyYW9sZGJhdWJrcmpwcGVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5ODI0NzMsImV4cCI6MjA2OTU1ODQ3M30.CrCKNy0UEGmfaAvveKbI72IadyU9xQi3D91BlMGomy4'
 
 let supabase: SupabaseClient | null = null;
 
