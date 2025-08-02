ef94cd96936f87bfb63221a136717317a498c7a8
Hamza Munir <infoathamza@gmail.com>
2025-08-02 02:56:46 +0000

    The `npm install` command failed in my project. Analyze the following er

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 import { Button } from '@/components/ui/button';
 
-pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 declare const __app_id: string;
 
