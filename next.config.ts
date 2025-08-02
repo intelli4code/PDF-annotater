0b764f6c6ab58ef60fe0807e9234451ee871e4a5
Hamza Munir <infoathamza@gmail.com>
2025-08-02 03:01:23 +0000

    This error message means that your web browser was unable to download a

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,4 @@
 import type {NextConfig} from 'next';
-import path from 'path';
-import CopyPlugin from 'copy-webpack-plugin';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -19,30 +17,6 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
-  },
-   webpack: (config, { isServer }) => {
-    // Correctly configure pdf.js worker
-    config.plugins.push(
-      new CopyPlugin({
-        patterns: [
-          {
-            from: path.join(
-              path.dirname(require.resolve('pdfjs-dist/package.json')),
-              'build/pdf.worker.min.js'
-            ),
-            to: path.join(config.output.path as string, 'static/chunks/'),
-          },
-        ],
-      })
-    );
-     // The `react-pdf` package needs this to be able to render PDFs.
-    config.resolve.alias['pdfjs-dist'] = path.join(
-      path.dirname(require.resolve('pdfjs-dist/package.json')),
-      'build/pdf.js'
-    );
-
-
-    return config;
   },
 };
 
